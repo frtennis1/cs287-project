75023dc2e8a1333203eba8d042c8dbeb71ad3d19
yling96 <yufeng.ling96@gmail.com>
2019-04-26 20:31:36 -0400

    Add quantization (very slow)

--- distortions.py
+++ distortions.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 
 from collections import OrderedDict
 
+from sklearn.cluster import MiniBatchKMeans
+
 def svd_compress(state_dict, k=100, tqdm=False):
     new_state_dict = OrderedDict()
     
@@ -45,4 +47,18 @@ def weight_prune(state_dict, p=.2, tqdm=False):
         new_state_dict[key] = mat
     
     return new_state_dict
+
+def quantization(state_dict, n_clusters=128):
+    flattened = torch.cat([param.flatten() for param in state_dict.values()]).cpu().numpy().reshape(-1, 1)
+    kmeans = MiniBatchKMeans(n_clusters=n_clusters, max_no_improvement=2).fit(flattened)
+    clustered = [kmeans.cluster_centers_[index] for index in kmeans.predict(flattened)]
+
+    # replace the original parameters with cluster centers
+    cursor = 0
+    for key in state_dict.keys():
+        shape = state_dict[key].shape
+        size = state_dict[key].numel()
+        state_dict[key] = torch.Tensor(clustered[cursor:cursor + size]).reshape(shape)
+        cursor += size
         
+    return state_dict
